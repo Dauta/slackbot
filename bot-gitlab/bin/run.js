6b65b33fa6208713de438a5c1d73720ecab99fae
Giorgi Rostomashvili <rostogiorgi@gmail.com>
2017-01-26 16:59:58 +0400

    Update run.js

--- bot-gitlab/bin/run.js
+++ bot-gitlab/bin/run.js
@@ -4,7 +4,7 @@ const service = require('../server/service');
 const http = require('http');
 
 const server = http.createServer(service);
-server.listen(3010);
+server.listen();
 
 server.on('listening', function() {
     console.log(`IRIS-Time is listening on ${server.address().port} in ${service.get('env')} mode.`);
